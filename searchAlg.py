c744d87e709cae23cec3f9e9deddf9e05b5f162f
Vladimir <paralogyX@gmail.com>
2020-10-18 18:09:52 +0200

    big improving. Problem with getting out (rec limit, overload etc)

--- searchAlg.py
+++ searchAlg.py
@@ -7,14 +7,23 @@ Created on Sun Oct 18 14:01:53 2020
 
 from theCubeGame2check import *
 
-setup = [['>', 'V', 'V', '<', '<', 'V'], 
-        ['>', '>', '>', 'V', 'V', 'V'],
-        ['>', 'V', 'V', '^', '<', '<'],
-        ['^', '>', '^', '<', '^', '<'],
-        ['^', '^', '<', '>', '<', '^'],
-        ['^', '>', '>', '^', '^', '<']]
+# setup = [['>', 'V', 'V', '<', '<', 'V'], 
+#         ['>', '>', '>', 'V', 'V', 'V'],
+#         ['>', 'V', 'V', '^', '<', '<'],
+#         ['^', '>', '^', '<', '^', '<'],
+#         ['^', '^', '<', '>', '<', '^'],
+#         ['^', '>', '>', '^', '^', '<']]
 
-def tryMove(curr_setup, moves):
+setup = [['>', 'V', 'V', '<', '<', 'V'], 
+        ['>', '^', '>', 'V', 'V', 'V'],
+        ['>', '^', 'V', '^', '<', '<'],
+        ['^', '^', '^', '<', '^', '<'],
+        ['^', '^', '<', '>', '<', '^'],
+        ['^', '^', '>', '^', '^', '<']]
+
+def tryMove(curr_setup, moves, moves_done):
+    if len(moves) == 0:
+        return "Empty"
     new_setup = copy.deepcopy(curr_setup)
     cue = moves
     move = cue.pop(0)
@@ -24,9 +33,17 @@ def tryMove(curr_setup, moves):
 
     tst = new_setup[t1][t2]
 
-    if new_setup[move[0]][move[1]] == '0':
-        new_setup[move[0]][move[1]] = setup[move[0]][move[1]]       #!!!Reading of global var
-        return cue
+    # if new_setup[move[0]][move[1]] == '0':
+    #     # print ("move: ", move, " 0: ", move[0], " 1: ", move[1])
+    #     # print (new_setup[move[0]][move[1]])
+    #     # print (setup[move[0]][move[1]])
+    #     new_setup[move[0]][move[1]] = setup[move[0]][move[1]]       #!!!Reading of global var
+    #     # print (new_setup[move[0]][move[1]])
+    #     # print (setup[move[0]][move[1]])
+    #     # new_setup[move[0]][move[1]] = 'XXX'
+    #     # print (new_setup[move[0]][move[1]])
+    #     # print (setup[move[0]][move[1]])
+    #     return cue
     
     num = selectable_number(new_setup,move[0],move[1])
     
@@ -41,17 +58,45 @@ def tryMove(curr_setup, moves):
         return True
     
     if num == 0:
-        return tryMove(new_setup,cue)
+        #move.extend(cue)
+        #Need previous move
+        if new_setup[move[0]][move[1]] == '0':
+            # print ("move: ", move, " 0: ", move[0], " 1: ", move[1])
+            # print (new_setup[move[0]][move[1]])
+            # print (setup[move[0]][move[1]])
+            new_setup[move[0]][move[1]] = setup[move[0]][move[1]]       #!!!Reading of global var
+            # print (new_setup[move[0]][move[1]])
+            # print (setup[move[0]][move[1]])
+            # new_setup[move[0]][move[1]] = 'XXX'
+            # print (new_setup[move[0]][move[1]])
+            # print (setup[move[0]][move[1]])
+            del moves_done[-1]
+            return tryMove(new_setup, cue, moves_done)
+        
+            #cue.insert(0, move)
+        else:
+            #cue = tryMove(new_setup,cue)
+            #newMoves = selectable(new_setup,move[0],move[1])
+            #newMoves.extend(cue)
+            #cue = newMoves
+            #cue.extend(newMoves)
+            #updatedSetup = select(new_setup,move[0],move[1])
+            
+            return tryMove(new_setup,cue,moves_done)
     else:
         newMoves = selectable(new_setup,move[0],move[1])
+        cue.insert(0,move)
         newMoves.extend(cue)
+        
         cue = newMoves
+        #cue.insert(0,move)
         #cue.extend(newMoves)
         updatedSetup = select(new_setup,move[0],move[1])
+        moves_done.append(move)
         
-        return tryMove(updatedSetup,cue)
+        return tryMove(updatedSetup,cue,moves_done)
 
         
         
 
-tst = tryMove(setup, [[1,1]])
+tst = tryMove(setup, [[0,0]], [])
